e65e8fa70c843e1bb99ec8a59e2361dd9ce46943
Jose Luis <joseluis27@ciencias.unam.mx>
2023-05-01 16:58:51 -0600

    Funcional

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,10 +1,29 @@
 
-let deck = []
+let deck
 
 const tipos = ['C','D','H','S']
 const monarcas = ['K','Q','J','A']
 
+const botonPedir = document.querySelector('#btnPedir')
+const botonDetener = document.querySelector('#btnDetener')
+const botonNuevo = document.querySelector('#btnNuevo')
+
+const puntajes = document.querySelectorAll('small')
+const divJugadorCartas = document.querySelector('#jugador-cartas')
+const divCompuCartas = document.querySelector('#computadora-cartas')
+
+let puntosJugador = 0
+let puntosComputadora = 0
+
+function fisherYatesShuffle(arr){
+    for(var i =arr.length-1 ; i>0 ;i--){
+        var j = Math.floor( Math.random() * (i + 1) );
+        [arr[i],arr[j]]=[arr[j],arr[i]];
+    }
+}
+
 const generarDeck = () => {
+    deck = []
 
     for (let index = 2; index <= 10; index++) {
         for (let tipo of tipos) {
@@ -18,7 +37,7 @@ const generarDeck = () => {
         }
     }
 
-    return _.shuffle(deck)
+    return fisherYatesShuffle(deck)
 }
 
 generarDeck()
@@ -36,5 +55,83 @@ const valorCarta = (carta) => {
     return (isNaN(valor)) ? ((valor === 'A') ? 11:10):valor*1
 }
 
-const val = valorCarta('8D')
-console.log(val)
+const turnoComputadora = (puntosMin) => {
+    
+    do {
+        const carta = getCarta()
+        puntosComputadora += valorCarta(carta)
+        puntajes[1].innerText = puntosComputadora
+
+        const imgCarta = document.createElement('img')
+        imgCarta.src = `assets/cartas/${carta}.png`
+        imgCarta.classList.add('carta')
+
+        divCompuCartas.append(imgCarta)
+
+        if (puntosMin > 21) {
+            break
+        }
+    
+    } while ((puntosComputadora < puntosMin) && (puntosMin <= 21));
+
+    setTimeout(() => {
+    
+        if (puntosMin === puntosComputadora ) {
+            alert('Empate!')
+        } else if (puntosMin > 21) {
+            alert('Computadora gana!')
+        } else if (puntosComputadora > 21 ) {
+            alert('Jugador gana!')
+        } else {
+            alert('Computadora gana!')
+        }
+    }, 100);
+}
+
+// Eventos
+botonPedir.addEventListener('click', () => {
+    
+    const carta = getCarta()
+    puntosJugador += valorCarta(carta)
+    puntajes[0].innerText = puntosJugador
+
+    const imgCarta = document.createElement('img')
+    imgCarta.src = `assets/cartas/${carta}.png`
+    imgCarta.classList.add('carta')
+
+    divJugadorCartas.append(imgCarta)
+
+    if (puntosJugador > 21) {
+        botonPedir.disabled = true
+        botonDetener.disabled = true
+        turnoComputadora(puntosJugador)
+    } else if (puntosJugador === 21 ){
+        botonPedir.disabled = true
+        botonDetener.disabled = true
+        turnoComputadora(puntosJugador)
+    }
+    
+})
+
+botonDetener.addEventListener('click', () => {
+    botonDetener.disabled = true
+    botonPedir.disabled = true
+    turnoComputadora(puntosJugador)
+})
+
+botonNuevo.addEventListener('click', () => {
+    
+    generarDeck()
+
+    puntajes[0].innerText = 0
+    puntajes[1].innerText = 0
+    
+    puntosComputadora = 0
+    puntosJugador = 0
+    
+    divJugadorCartas.innerHTML = ''
+    divCompuCartas.innerHTML =''
+
+    botonPedir.disabled = false
+    botonDetener.disabled = false
+})

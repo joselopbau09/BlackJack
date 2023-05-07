1b2443d039c41ae9a561abd70a4995515a537030
Jose Luis <joseluis27@ciencias.unam.mx>
2023-05-06 21:26:35 -0600

    Programa funcional

--- src/blackjack/index.js
+++ src/blackjack/index.js
@@ -1,9 +1,11 @@
 import _ from "underscore";
-import {generarDeck} from "./usecases/createDeck";
-import {getCarta} from "./usecases/getCarta";
-import {valorCarta} from "./usecases/valorCarta";
 
-let deck
+import { generarDeck, getCarta, valorCarta, turnoComputadora, crearCartaHTML } from './usecases';
+
+let deck = []
+
+let puntosJugador = 0
+let puntosComputadora = 0
 
 const tipos = ['C','D','H','S']
 const monarcas = ['K','Q','J','A']
@@ -16,70 +18,29 @@ const puntajes = document.querySelectorAll('small')
 const divJugadorCartas = document.querySelector('#jugador-cartas')
 const divCompuCartas = document.querySelector('#computadora-cartas')
 
-let puntosJugador = 0
-let puntosComputadora = 0
-
 
 deck = generarDeck(tipos,monarcas)
-
-
-
-
-const turnoComputadora = (puntosMin) => {
-    
-    do {
-        const carta = getCarta(deck)
-        puntosComputadora += valorCarta(carta)
-        puntajes[1].innerText = puntosComputadora
-
-        const imgCarta = document.createElement('img')
-        imgCarta.src = `assets/cartas/${carta}.png`
-        imgCarta.classList.add('carta')
-
-        divCompuCartas.append(imgCarta)
-
-        if (puntosMin > 21) {
-            break
-        }
-    
-    } while ((puntosComputadora < puntosMin) && (puntosMin <= 21));
-
-    setTimeout(() => {
-    
-        if (puntosMin === puntosComputadora ) {
-            alert('Empate!')
-        } else if (puntosMin > 21) {
-            alert('Computadora gana!')
-        } else if (puntosComputadora > 21 ) {
-            alert('Jugador gana!')
-        } else {
-            alert('Computadora gana!')
-        }
-    }, 100);
-}
 
 // Eventos
 botonPedir.addEventListener('click', () => {
     
     const carta = getCarta(deck)
-    console.log(deck)
+    
     puntosJugador += valorCarta(carta)
     puntajes[0].innerText = puntosJugador
 
-    const imgCarta = document.createElement('img')
-    imgCarta.src = `assets/cartas/${carta}.png`
-    imgCarta.classList.add('carta')
-
+    const imgCarta = crearCartaHTML(carta)
     divJugadorCartas.append(imgCarta)
 
     if (puntosJugador > 21) {
         botonPedir.disabled = true
         botonDetener.disabled = true
-        turnoComputadora(puntosJugador)
+        turnoComputadora(puntosJugador, puntajes[1], divCompuCartas, deck)
+
     } else if (puntosJugador === 21 ){
         botonPedir.disabled = true
         botonDetener.disabled = true
-        turnoComputadora(puntosJugador)
+        turnoComputadora(puntosJugador, puntajes[1], divCompuCartas, deck)
     }
     
 })
@@ -87,18 +48,20 @@ botonPedir.addEventListener('click', () => {
 botonDetener.addEventListener('click', () => {
     botonDetener.disabled = true
     botonPedir.disabled = true
-    turnoComputadora(puntosJugador)
+
+    turnoComputadora(puntosJugador, puntajes[1], divCompuCartas, deck)
 })
 
 botonNuevo.addEventListener('click', () => {
     
     deck = generarDeck(tipos, monarcas)
+    
+    puntosComputadora = 0
+    puntosJugador = 0
 
     puntajes[0].innerText = 0
     puntajes[1].innerText = 0
     
-    puntosComputadora = 0
-    puntosJugador = 0
     
     divJugadorCartas.innerHTML = ''
     divCompuCartas.innerHTML =''
---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,6 @@
+
+export { crearCartaHTML } from './crearCartaHTML';
+export { generarDeck} from './createDeck';
+export { getCarta } from './getCarta';
+export { turnoComputadora } from './turnoCompuadora';
+export { valorCarta } from './valorCarta';
